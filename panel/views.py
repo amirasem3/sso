ff1e4bd7b16fe174eeac1cfbf04125698a68fe55
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2020-01-05 11:20:24 +0330

    debuging in order to check profile

--- panel/views.py
+++ panel/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,8 +6,19 @@ from accounting.models import *
 from panel.forms import *
 
 
+@login_required
 def panel(request):
-    return render(request, 'panel.html')
+    if request.method == 'GET':
+        user = request.user
+        profile = Profile.objects.filter(user=user).first()
+        user = request.user
+        profile = Profile.objects.filter(user=user).first()
+        return render(request, 'panel.html', {"profile": profile})
+    else:
+#         post request
+        pass
+#     reload the inputs to profile
+#         todo read inputs and add image inputs to template
 
 
 def get_profile(request):
@@ -18,22 +30,22 @@ def get_profile(request):
             form = ProfileForm(instance=profile)
             print("after from creation ***")
 
-            return render(request, 'profile.html', {'form': form})
+            return render(request, 'profile.html', {'form': form, 'profile': profile})
         else:
             form = ProfileForm(instance=Profile())
             return render(request, 'profile.html', {'form': form})
 
     else:
-        form = ProfileForm(request.POST,instance=profile)
-        print(form.is_valid(),"**In post **")
+        form = ProfileForm(request.POST, instance=profile)
+        print(form.is_valid(), "**In post **")
         if form.is_valid():
+            # save image
+
             form.save()
-        return render(request, 'profile.html', {'form': form})
 
 
 
-
-
+        return render(request, 'profile.html', {'form': form, 'profile': profile})
 
 # def sign_up(request):
 #     user = request.user
